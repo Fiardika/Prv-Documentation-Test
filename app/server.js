4ec47254342e64e1f4d46af831319100529f35e2
fiarzk <fiardikarzk@gmail.com>
2022-11-10 14:49:05 +0700

    change config file

--- app/server.js
+++ app/server.js
@@ -1,13 +1,11 @@
 const express = require("express");
-require("dotenv").config();
-
 const app = express();
 const port = process.env.PORT || 3000;
+require("dotenv").config();
 
-app.get("/", function (req, res) {
-    console.log(req.socket.remoteAddress);
-    console.log(req.ip);
-    res.send("IP: " + req.ip);
+app.get('/',function(req, res) {
+    const ipAddresses = req.header('x-forwarded-for');
+    res.send("Your IP Address: " + ipAddresses);
 });
 
 app.listen(port, () => {
